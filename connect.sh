8ac41468599a4cc592581d99f03a2bd8ac2b0bd6
aldmbmtl <alex.e.hatfield@gmail.com>
2024-10-15 12:39:51 -0400

    Made it work with env instead

--- connect.sh
+++ connect.sh
@@ -1,16 +1,12 @@
 cp -v /config.sshd /etc/ssh/ssh_config.d/lic.conf
 chmod -v 640 /etc/ssh/ssh_config.d/lic.conf
-
-ssh -v -4 -t \
-  -o StrictHostKeyChecking=no\
-  -o ForwardAgent=yes\
-  -o TCPKeepAlive=yes\
-  -L 4101:0.0.0.0:4101 \
-  -L 4102:0.0.0.0:4102 \
-  -L 37172:0.0.0.0:37172 \
-  -i /key.pem centos@54.242.3.91 \
-  'while true; do sleep 2; done'&
-
+ports=("${PORTS//,/ }")
+IFS=',' read -r -a ports <<< "$PORTS"
+FRD=""
+for port in "${ports[@]}"; do
+  FRD+=" -L $port:0.0.0.0:$port"
+done
+ssh -v -4 -t -o StrictHostKeyChecking=no $FRD -i /key.pem "$USER@$HOST" 'while true; do sleep 2; done'&
 while true;
 do
   sleep 2
